c0732462153fc4407646f01da26c4c6e308dc2ca
CSWILIK <cswilik@gmail.com>
2021-03-31 15:31:57 -0400

    add rotate array

---
+++ leetcode_questions/rotate_array.js
@@ -0,0 +1,34 @@
+// Rotate array - given array, rotate it to the right 'k' amount of steps, where k is non-negative.
+// 
+
+
+// My solution. So in the actual directions, it does not say you *can't* create a new array...
+// So this does work.
+var rotate = function(nums, k) {
+    let results = [];
+    let mid = nums.length - k
+    let left = nums.slice(0, mid)
+    let right = nums.slice(mid)
+    results = right.concat(left);
+    return results;
+};
+
+rotate([1,2,3,4,5,6,7], 3)
+
+
+// However, to do this without creating a new array:
+
+var rotate = function(nums, k) {
+    let rotatePoint = k % nums.length
+    for(i = rotatePoint; i > 0; i --) {
+        let val = nums.pop()
+        nums.unshift(val)
+    }
+    return nums
+}
+
+rotate([1,2,3,4,5,6,7], 3)
+
+
+// I like my version better!!!! haha.
+
